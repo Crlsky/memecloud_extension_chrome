4ab13661c9afe9865a0f5963c2e997e730ca0569
Crlsky <karol.swieciochowski@gmail.com>
2021-01-21 06:42:37 +0100

    add lib

--- memonator.js
+++ memonator.js
@@ -31,70 +31,76 @@ function getContent(id_parent = null) {
 }
 
 $(document).ready(function(){
-    renderMemeCloudPanel();
-    makeButtonForImages();
-    makeButtonForLazyLoadedImgs();
-})
+    let arr = document.getElementsByTagName('img');
 
-function makeButtonForImages(){
-    $('img').each(function(){
-        let btn = document.createElement("dev");
+    for (let index = 0; index < arr.length; ++index) {
+        let btn = document.createElement("a");
         btn.innerHTML= "M";
         btn.setAttribute('class','memeCloud-addbtn');
-        btn.setAttribute('data-url', $(this).attr('src'));
-        btn.setAttribute('width', $(this).width());
-        btn.setAttribute('heigth', $(this).height());
+        btn.setAttribute('href','###');
+        btn.setAttribute('data-url', arr[index].getAttribute('src'));
 
-        if($(this).width() > 250 && $(this).height() > 250)
-            $(this).closest('a').parent().append(btn);
-        
-    });
-}
+        if(arr[index].width > 250 && arr[index].height > 250)
+            arr[index].parentElement.appendChild(btn);
+    }
 
-function makeButtonForLazyLoadedImages() {
-    $('img').on('load', function(){
-        let btn = document.createElement("dev");
-        btn.innerHTML= "M";
-        btn.setAttribute('class','memeCloud-addbtn');
-        btn.setAttribute('data-url', $(this).attr('src'));
-        btn.setAttribute('width', $(this).width());
-        btn.setAttribute('heigth', $(this).height());
-    
-        if($(this).width() > 250 && $(this).height() > 250)
-            $(this).closest('a').parent().append(btn);
-        
-    });
-}
+    let box = $('<div id="memeCloud" style="right: -320px;"></div>').html('<button class="btn btn-primary memeCloud-sidetab" data-toggle="modal" data-target="#memeCloud-panel" >+</button>'
+                                                                            +'<div class="modal memeCloud-panel" id="memeCloud-panel" tabindex="-1" role="dialog">'
+                                                                                +'<div class="modal-dialog">'
+                                                                                    +'<div class="modal-content">'
+                                                                                        +'<div class="modal-header">'
+                                                                                    
+                                                                                            +'<img class="memeCloud-logo" src="https://memecloud.co/assets/img/logo_extension.png"/>'
+                                                                                            +'<canvas id="memeCloud-canvas" crossorigin="anonymous"></canvas>'
+                                                                                            +'<h2>MEMECLOUD.CO</h2>'
+                                                                                        
+                                                                                        +'</div>'
 
-function renderMemeCloudPanel() {
-    let box = $('<div id="memeCloud" style="right: -320px;"></div>').html('<div class="memeCloud-sidetab">M</div>'
-                                                                            +'<div class="memeCloud-panel">'
-                                                                                +'<img class="memeCloud-logo" src="https://memecloud.co/assets/img/logo_extension.png"/>'
-                                                                                +'<canvas id="memeCloud-canvas" crossorigin="anonymous"></canvas>'
-                                                                                +'<h2>MEMECLOUD.CO</h2>'
-                                                                                +'<input type="hidden" class="memeCloud-currentLocalization" value="-420" />'
-                                                                                +'<div class="memeCloud-nav">'
-                                                                                    +'<button class="memeCloud-backButton">⬅</button>'
-                                                                                    +'<button class="memeCloud-logoutButton"> 🗝 </button>'
-                                                                                +'</div>'
-                                                                                +'<div class="memeCloud-localizations"></div>'
-                                                                                +'<div class="memeCloud-memes"></div>'
-                                                                                +'<div class="memeCloud-form">'
-                                                                                    +'<form>'
-                                                                                        +'<input type="text" id="memeCloud-login" class="memeCloud-formfield"  placeholder="Login" /><hr>'
-                                                                                        +'<input type="password" id="memeCloud-passwd" class="memeCloud-formfield" placeholder="Password" autocomplete="on" /><hr>'
-                                                                                        +'<button class="memeCloud-btn">Sing In</buton>'
-                                                                                    +'</form>'
+                                                                                        +'<div class="modal-body">'
+
+                                                                                            +'<input type="hidden" class="memeCloud-currentLocalization" value="-420" />'
+                                                                                            +'<div class="memeCloud-nav">'
+                                                                                                +'<button class="memeCloud-backButton">⬅</button>'
+                                                                                                +'<button class="memeCloud-logoutButton"> 🗝 </button>'
+                                                                                            +'</div>'
+                                                                                            +'<div class="memeCloud-localizations"></div>'
+                                                                                            +'<div class="memeCloud-memes"></div>'
+                                                                                            +'<div class="memeCloud-form">'
+                                                                                                +'<form>'
+                                                                                                    +'<input type="text" id="memeCloud-login" class="memeCloud-formfield"  placeholder="Login" /><hr>'
+                                                                                                    +'<input type="password" id="memeCloud-passwd" class="memeCloud-formfield" placeholder="Password" autocomplete="on" /><hr>'
+                                                                                                    +'<button class="memeCloud-btn">Sing In</buton>'
+                                                                                                +'</form>'
+                                                                                            +'</div>'
+
+                                                                                        +'</div>'
+                                                                                        +'<div class="modal-footer">'
+                                                                                        +'</div>'
+                                                                                    +'</div>'
                                                                                 +'</div>'
                                                                             +'</div>');
     $('body').append(box);
-}
 
+    //new img lazy loaded or something like that
+    $('img').on('load', function(){
+        let btn = document.createElement("a");
+        btn.innerHTML= "M";
+        btn.setAttribute('class','memeCloud-addbtn');
+        btn.setAttribute('href','###');
+        btn.setAttribute('data-url', $(this).attr('src'));
+
+        if($(this).width() > 250 && $(this).height() > 250){
+            $(this).parent().append(btn);
+            console.log($(this).width());
+        }
+    });
+})
 
 $(document).on('click', '.memeCloud-addbtn', function(event){
     event.preventDefault();
+    
     let url = $(this).data('url');
-    let name = prompt("Podaj nazwe mema");
+    let name = prompt("Podaj nawzwe mema");
 
     let data = {
                 url: url,
